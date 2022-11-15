bba153a0b19b02e7295c319922116125c03609d9
Marcelo Victor <marcelo.victor@aluno.ifsertao-pe.edu.br>
2022-11-15 09:12:48 -0300

    Update 2 Home/Sobre

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-﻿<?php
-=======
 <?php
->>>>>>> 537ec8fc1044224bbfe455f868916e49a9f38843
 include "PHP/conexao.php";
 session_start();
 ?>
@@ -17,11 +13,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/stylegeral.css">
     <link rel="stylesheet" href="CSS/button.css">
-<<<<<<< HEAD
     <link rel="icon" type="image/png" href="..\Midia\Img\puzzle-removebg-preview.png" />
-=======
-    <link rel="icon" type="image/png" href="Midia\Img\puzzle-removebg-preview.png" />
->>>>>>> 537ec8fc1044224bbfe455f868916e49a9f38843
 </head>
 <body class="font-sigmar bg-fundosite">
     <section class="container mt-4">
@@ -47,35 +39,58 @@ session_start();
         </header>
 
         <main>
-            <div class="row">
-                <div class="d-none d-lg-block col-lg-3">
-                    <img class="img-fluid" src="Midia/Img/puzzle-removebg-preview.png" alt="">
-                </div>
-                <div class="col-lg-9 lead">
-                    <p class="text-center my-3 text-light">
-                        Puzzle Brain é um jogo educacional desenvolvido por estudantes do ensino médio do IF-Sertão.
-                        Seu Objetivo é ensinar os estudantes e público em geral, conceitos importantes de História, Matemática e Português, que posteriormente, poderá
-                        ajuda-lós em trabalhos escolares e até mesmo, no mercado de trabalho.
-                    </p>
-                </div>
-            </div>
+         
 
             <div class="my-4 btn-group my-5 d-flex align-content-center justify-content-center gap-1 flex-wrap">
                 <a class="btn btn-primary bg-botao-versao2 btn btn-outline-light btn-lg" href="PHP/game.php"> Jogar </a>
                 <a class="btn btn-primary bg-botao-versao2 btn btn-outline-light btn-lg" href="PHP/ranking.php">Ver Rank</a> 
                 <a class="btn btn-primary bg-botao-versao2 btn btn-outline-light btn-lg" href="PHP/cadastro.php">Fazer Cadastro</a>
                 <a class="btn btn-primary bg-botao-versao2 btn btn-outline-light btn-lg" href="PHP/login.php">Fazer Login</a>
-<<<<<<< HEAD
-                <a class="btn btn-primary bg-botao-versao2 btn btn-outline-light btn-lg" href="PHP/sobre.html">Saiba Mais</a>
-=======
                 <a class="btn btn-primary bg-botao-versao2 btn btn-outline-light btn-lg" href="PHP/sobre.php">Saiba Mais</a>
->>>>>>> 537ec8fc1044224bbfe455f868916e49a9f38843
 
             </div>
         </main>
     </section>
 
+
+
+    
+
+  <footer>
+      <hr style="margin-top:50px;" />
+      <table class="table" class="text-center my-3 text-light" >
+        <thead>
+            <tr><th class="text-center my-3 text-light" colspan="4"> Contato e redes sociais</th> </tr>
+        </thead> 
+        
+     
+    
+  </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    
+    <div class="row">
+        
+        <div class="col-lg-9 lead">
+            
+    <style>
+    table, td, th, tfoot {border: solid 1px #5c65c0;padding: 5px;}
+    th {background-color: #5c65c0;}
+    caption {font-size: x-large;}
+   
+    </style>
+        
+        <table class="table" class="text-center my-3 text-light" >
+    
+        <tbody>
+        <tr><td  class="text-center my-3 text-light" colspan="4">
+            © 2019-2022 Puzzle Brain Co., Inc. Todos os direitos reservados.
+        </td></tr>
+    </tfoot> 
+    </table>
+            
+        </div>
+    </div>
+</footer>
 </body>
 </html>
 
